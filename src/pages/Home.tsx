e631562dd4c11a3286fc769ce0c15d12c8d51fa5
actjacob <yakupaktar99@gmail.com>
2025-06-17 09:24:01 +0300

    Remove unused features array and related imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,37 +12,6 @@ import {
   Paper,
   TextField,
 } from '@mui/material';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import TimerIcon from '@mui/icons-material/Timer';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-
-const features = [
-  {
-    icon: <RestaurantIcon sx={{ fontSize: 40, color: 'primary.main' }} />,
-    title: 'Up to 365 days/year',
-    description:
-      "Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that's your style.",
-  },
-  {
-    icon: <TimerIcon sx={{ fontSize: 40, color: 'primary.main' }} />,
-    title: 'Ready in 20 minutes',
-    description:
-      "You're only twenty minutes away from your delicious and super healthy meals delivered right to your home. We work with the best chefs in each town to ensure that you're 100% happy.",
-  },
-  {
-    icon: <LocalShippingIcon sx={{ fontSize: 40, color: 'primary.main' }} />,
-    title: '100% organic',
-    description:
-      'All our vegetables are fresh, organic and local. Animals are raised without added hormones or antibiotics. Good for your health, the environment, and it also tastes better!',
-  },
-  {
-    icon: <FavoriteIcon sx={{ fontSize: 40, color: 'primary.main' }} />,
-    title: 'Order anything',
-    description:
-      "We don't limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It's up to you!",
-  },
-];
 
 const meals = [
   {
